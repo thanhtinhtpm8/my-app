9226a59064150106c4c9997113ebde4004470ce0
tinhtt <tinhtt>
2023-12-22 14:30:25 +0700

    update

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { RoomService } from 'src/app/core/services/room.service';
 import { UserService } from 'src/app/core/services/user.service';
 import { environment } from 'src/environments/environment';
 import * as signalR from '@microsoft/signalr';  
 import { AuthService } from 'src/app/core/services/auth.service';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
+const mediaConstraints = {
+  audio: true,
+  video: {width: 1280, height: 720}
+  // video: {width: 1280, height: 720} // 16:9
+  // video: {width: 960, height: 540}  // 16:9
+  // video: {width: 640, height: 480}  //  4:3
+  // video: {width: 160, height: 120}  //  4:3
+};
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit ,AfterViewInit {
+
+  @ViewChild('local_video') local_video:any= ElementRef;
+  @ViewChild('received_video') remoteVideo:any= ElementRef;
+
+  //
+  private localStream:any = MediaStream;
+
+
   isVisible = false;
   allUser:any=[];
   myInfor:any;
@@ -20,6 +37,9 @@ export class HomePageComponent implements OnInit {
     private userService:UserService,
     private authService:AuthService,
     private notification: NzNotificationService){}
+  ngAfterViewInit(): void {
+    this.requestMediaDevice()
+  }
   ngOnInit(): void {
       this.loadRoom();
       this.loadAllUser();
@@ -66,5 +86,10 @@ export class HomePageComponent implements OnInit {
       title,
       content
     );
+  }
+
+  //
+  private async requestMediaDevice(): Promise<void>{
+     this.localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
   }
 }
